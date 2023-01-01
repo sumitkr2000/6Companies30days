7239011c11866056afc96037383c28a138c81854
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-01-01 19:44:28 +0530

    Create Bulls and Cows.cpp

---
+++ Challenge Company 1: Microsoft/Bulls and Cows.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        
+        vector<int> v1(10, 0), v2(10, 0);
+        int b = 0, c = 0;
+        
+        for(int i = 0; i < guess.size(); i++) {
+            if(secret[i] == guess[i]) {
+                b++;
+            }
+            else {
+                v1[secret[i]-'0']++;
+                v2[guess[i]-'0']++;
+            }
+        }
+        
+        for(int i = 0; i < 10; i++) {
+            if(v1[i] != 0 && v2[i] != 0) {
+                c += min(v1[i], v2[i]);
+            }
+        }
+        
+        return to_string(b) + "A" + to_string(c) + "B";
+    }
+};

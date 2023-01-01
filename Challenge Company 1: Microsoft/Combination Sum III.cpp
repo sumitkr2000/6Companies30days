3474efbb29b71698d599362c72b215e9147b40c9
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-01-01 19:37:13 +0530

    Create Combination Sum III.cpp

---
+++ Challenge Company 1: Microsoft/Combination Sum III.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    void solve(int num, int k, int n, vector<int> &curr, vector<vector<int>> &ans) {
+        
+        if(n == 0 && curr.size() == k) {
+            ans.push_back(curr);
+            return;
+        }
+        
+        for(int i = num; i <= 9; i++) {
+            curr.push_back(i);
+            solve(i+1, k, n-i, curr, ans);
+            curr.pop_back();
+        }
+    }
+    
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        vector<int> curr;
+        vector<vector<int>> ans;
+        
+        solve(1, k, n, curr, ans);
+        return ans;
+    }
+};
